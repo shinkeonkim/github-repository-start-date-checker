49e6899703eddbf3ed53c144defce747d2abf91d
shinkeonkim <singun11@kookmin.ac.kr>
2021-09-26 21:40:49 +0900

    feat: 팀 번호, 팀명, 시작일자도 데이터에 포함

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# coding: utf8
 import asyncio, aiohttp, aiofiles, maya
 
 from datetime import datetime
@@ -6,7 +7,7 @@ VALID_DATE = datetime(2021,9,22).date()
 TOKEN = open('TOKEN', 'r').readline()
 HEADERS = {'Accepts': 'application/json', 'Authorization': f'token {TOKEN}'}
 
-async def get_repository_info(repository, api_url):
+async def get_repository_info(team_number, team_name, repository, api_url):
   async with aiohttp.ClientSession(headers=HEADERS) as session:
     async with session.get(api_url) as response:
       response = await response.json()
@@ -15,20 +16,25 @@ async def get_repository_info(repository, api_url):
       try:
         created_at = maya.parse(response['created_at']).datetime().date()
       except:
+        created_at = 'ERROR'
         is_valid_repository = False
 
-      if VALID_DATE > created_at or not is_valid_repository or response['fork']:
-        async with aiofiles.open('invalid_github_repositories.csv', 'a') as f:
+      if not is_valid_repository or VALID_DATE > created_at or response['fork']:
+        async with aiofiles.open('invalid_github_repositories.csv', 'a',encoding='utf8') as f:
           await f.write(repository)
+      async with aiofiles.open('results.csv', 'a',encoding='utf8') as f:
+        await f.write(",".join([team_number, team_name, str(created_at), repository]))
 
       return response
 
 
-repositories = open('github_urls.csv','r').readlines()
+repositories = open('github_urls.csv','r',encoding='utf8').readlines()
 tasks = set()
 
-for repository in repositories:
+for repository_info in repositories:
+  team_number, team_name, repository = repository_info.split(',')
+  
   repository_api_url = repository.replace('https://github.com/', 'https://api.github.com/repos/')
-  tasks.add(get_repository_info(repository, repository_api_url))
+  tasks.add(get_repository_info(team_number, team_name, repository, repository_api_url))
 
 asyncio.run(asyncio.wait(tasks))
